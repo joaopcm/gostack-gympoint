5de729fdd738e8dba123dc29859a72c8d0e7ae62
Joao Melo <jopcmelo@gmail.com>
2019-11-11 16:24:41 -0300

    feat: Add active virtual field to enrollment model

--- backend/src/app/models/Enrollment.js
+++ backend/src/app/models/Enrollment.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import { isBefore, isAfter } from 'date-fns';
 
 class Enrollment extends Model {
   static init(sequelize) {
@@ -9,6 +10,18 @@ class Enrollment extends Model {
         start_date: Sequelize.DATE,
         end_date: Sequelize.DATE,
         price: Sequelize.DECIMAL,
+        active: {
+          type: Sequelize.VIRTUAL(Sequelize.BOOLEAN, [
+            'start_date',
+            'end_date',
+          ]),
+          get() {
+            return (
+              isBefore(this.get('start_date'), new Date()) &&
+              isAfter(this.get('end_date'), new Date())
+            );
+          },
+        },
       },
       { sequelize }
     );
