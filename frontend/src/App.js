9952977141d81cbdf5d01d081086b6f651dbf00a
Joao Melo <jopcmelo@gmail.com>
2019-11-20 17:13:39 -0300

    chore: Add store to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/reactotronConfig';
@@ -6,13 +7,17 @@ import './config/reactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
+import store from './store';
+
 import GlobalStyles from './styles/global';
 
 export default function src() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyles />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyles />
+      </Router>
+    </Provider>
   );
 }
