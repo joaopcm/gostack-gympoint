dd8f63f37eac7a3ab78c1f140327bca1d88959a0
Joao Melo <jopcmelo@gmail.com>
2019-11-20 14:51:42 -0300

    chore: Add reactotron bridge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 
+import './config/reactotronConfig';
+
 import Routes from './routes';
 import history from './services/history';
 
