f708954606a4333f1f54bc4105b1e12e31f5e7f0
Joao Melo <jopcmelo@gmail.com>
2019-11-20 17:49:29 -0300

    chore: Saving profile

--- frontend/src/store/modules/rootReducer.js
+++ frontend/src/store/modules/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 
 import auth from './auth/reducer';
+import user from './user/reducer';
 
-export default combineReducers({ auth });
+export default combineReducers({ auth, user });
