77bf56439a6394411b12f4bf56acb332dcf7b5be
Joao Melo <jopcmelo@gmail.com>
2019-11-20 18:09:52 -0300

    feat: Add auth loading

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -1,23 +1,27 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 import api from '~/services/api';
 import history from '~/services/history';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  yield put(signInSuccess(token, user));
+    yield put(signInSuccess(token, user));
 
-  history.push('/dashboard');
+    history.push('/dashboard');
+  } catch (error) {
+    yield put(signFailure());
+  }
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
