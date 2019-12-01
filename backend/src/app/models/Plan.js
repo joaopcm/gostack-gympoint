55704a122fb4a370e9faf32b3ed376a12ffc9d46
João Melo <jopcmelo@gmail.com>
2019-12-01 17:24:30 -0300

    feat: Add a virtual field to calc total price at plan model

--- backend/src/app/models/Plan.js
+++ backend/src/app/models/Plan.js
@@ -7,6 +7,12 @@ class Plan extends Model {
         title: Sequelize.STRING,
         duration: Sequelize.INTEGER,
         price: Sequelize.DECIMAL,
+        total_price: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return this.price * this.duration;
+          },
+        },
       },
       { sequelize }
     );
